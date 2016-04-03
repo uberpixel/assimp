b4acf1f96a397f1646b3d276520f4ef61d13f7f5
Sidney Just <sidney.just@googlemail.com>
2016-04-03 13:32:06 +0100

    Fixes issue with MingW/CLion where M_PI is not defined

--- contrib/poly2tri/poly2tri/common/utils.h
+++ contrib/poly2tri/poly2tri/common/utils.h
@@ -38,6 +38,11 @@
 #include <exception>
 #include <math.h>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#define M_PI_2 1.57079632679489661923
+#endif
+
 namespace p2t {
 
 const double PI_3div4 = 3 * M_PI / 4;
